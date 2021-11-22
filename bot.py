200b04476c51f909cc63dc12fb0589725a6ddff6
Jorge Celaya <jcelaya775@gmail.com>
2021-11-22 16:27:51 -0600

    initial commit

--- bot.py
+++ bot.py
@@ -1,10 +1,19 @@
+import os
 import cbpro
 import json
+from dotenv import load_dotenv
 import pandas as pd
 from coinbase.wallet.client import Client
 
-auth_client = cbpro.AuthenticatedClient(
-    "384297671372c0a0c6301bcb7913ac8c", "rXEJogcEz5k+hIPLHdigM1mjuYuv4EkRku6mHUeh6fYip02dpxcaHvcEeQ7VxEr7EugGO4Dl+WGz57pLmt7bCg==", "a71nvq6710v")
+load_dotenv("auth_credentials.env")
+api_key = os.getenv("API_KEY")
+api_secret = os.getenv("API_SECRET")
+api_pass = os.getenv("API_PASS")
+
+print(api_key)
+print(api_secret)
+print(api_pass)
+auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass)
 
 # data = pd.DataFrame(public_client.get_products())
 # print(data.tail(6).T)
@@ -16,7 +25,7 @@ for account in accounts:
     if float(account["balance"]) != 0:
         print(json.dumps(account, indent=2))
 
-account_holds = auth_client.get_account_holds(before=2)
-print(json.dumps(account_holds, indent=2))
+# account_holds = auth_client.get_account_holds()
+# print(json.dumps(account_holds, indent=2))
 
 # print(json.dumps(auth_client.get_currencies(), indent=4))
