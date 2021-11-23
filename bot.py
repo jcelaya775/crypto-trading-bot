a2d2fc39952adcda1f2edeec5d8d818f0fca4767
Jorge Celaya <jcelaya775@gmail.com>
2021-11-23 00:53:15 -0600

    implemented a coinbase wallet class

--- bot.py
+++ bot.py
@@ -1,58 +1,55 @@
-import cbpro
+import cbpro, pandas as pd, hmac, json, os, hashlib, time
 from requests.auth import AuthBase
-import pandas as pd
-import hmac
-import json
-import os
 from dotenv import load_dotenv
-import hashlib
-import time
 
 
-class CoinbaseWalletAuth(AuthBase):
-    def __init__(self, api_key, secret_key):
-        self.api_key = api_key
-        self.secret_key = secret_key
+class CoinbaseWallet(AuthBase):
+    def __init__(self, api_key, api_secret, api_pass):
+        self.auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass)
+        self.positions = []
+        stable_coins = []
+        
+    def __call__ (self):
+        print(json.dumps(self.auth_client.get_product_ticker("BTC-USD"), indent=2))
+        
+    def getPositions(self):
+        accounts = self.auth_client.get_accounts()
+        
+        for account in accounts:
+            if float(account["balance"]) != 0:
+                self.positions.append(account)
 
-    def __call__(self, request):
-        timestamp = str(int(time.time()))
-        message = timestamp + request.method + \
-            request.path_url + (request.body or '')
-        signature = hmac.new(self.secret_key, message,
-                             hashlib.sha256).hexdigest()
-
-        request.headers.update({
-            'CB-ACCESS-SIGN': signature,
-            'CB-ACCESS-TIMESTAMP': timestamp,
-            'CB-ACCESS-KEY': self.api_key,
-        })
-        return request
+        print(json.dumps(self.positions, indent=2))
+    
+    def getStableCoins(self):
+        currencies = self.auth_client.get_products()
+        
+        for currency in currencies:
+            if currency["fx_stablecoin"] and currency["quote_currency"] == "USD":
+                print(json.dumps(currency, indent=2))
 
 
-# get authentication variables
-load_dotenv("auth_credentials.env")
-api_key = os.getenv("API_KEY")
-api_secret = os.getenv("API_SECRET")
-api_pass = os.getenv("API_PASS")
+def main():
+    # get authentication variables
+    load_dotenv("auth_credentials.env")
+    api_key = os.getenv("API_KEY")
+    api_secret = os.getenv("API_SECRET")
+    api_pass = os.getenv("API_PASS")
+    
+    account = CoinbaseWallet(api_key, api_secret, api_pass)
+    account.getStableCoins()
+    account()
 
-auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass)
+    
+    # data = pd.DataFrame(public_client.get_products())
+    # print(data.tail(6).T)
 
-# data = pd.DataFrame(public_client.get_products())
-# print(data.tail(6).T)
+    # account_holds = auth_client.get_account_holds()
+    # print(json.dumps(account_holds, indent=2))
 
-cb_access_time = auth_client.get_time()
-
-api_secret = cbpro.AuthenticatedClient(api_key, api_secret, api_pass)
-
-accounts = auth_client.get_accounts()
-for account in accounts:
-    if float(account["balance"]) != 0:
-        print(json.dumps(account, indent=2))
-
-# account_holds = auth_client.get_account_holds()
-# print(json.dumps(account_holds, indent=2))
-
-# print(json.dumps(auth_client.get_currencies(), indent=4))
+    # print(json.dumps(auth_client.get_currencies(), indent=4))
+    
+main()
 
 # 1. Loop through each of our specified currencies
 # 2. Calculate the lookback length based on the volatility(higher volatility, higher lookback length)
