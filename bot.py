b4f4bfb061b3e6cc2c2e931fb8c529ba3e7656a8
Jorge Celaya <jcelaya775@gmail.com>
2021-11-25 17:07:50 -0600

    added variables and furthere implementation

--- bot.py
+++ bot.py
@@ -1,66 +1,102 @@
-import cbpro, pandas as pd, hmac, json, os, hashlib, time
+import cbpro
+from cbpro.websocket_client import WebsocketClient
+import pandas as pd
+import numpy as np
+import hmac
+import json
+import os
+import hashlib
+import time
+import datetime
 from requests.auth import AuthBase
 from dotenv import load_dotenv
 
 
+class TextWebSocketClient(cbpro.WebsocketClient):
+    def on_open(self):
+        self.url = 'wss://ws-feed.pro.coinbase.com/'
+        self.message_count = 0
+        print('hello world')
+
+    def on_message(self, msg):
+        self.message_count += 1
+        msg_type = msg.get('type', None)
+        if msg_type == 'ticker':
+            time_val = msg.get('time', ('-' * 27))
+            price_val = msg.get('price', None)
+            prive_val = float(
+                price_val) if price_val is not None else 'None'
+            product_id = msg.get('product_id', None)
+        print(
+            f'{time_val:30} {price_val:.3f} {product_id}\tchannel type: {msg_type}')
+
+    def on_close(self):
+        print(
+            f'<---Websocket connection closed--->\n\tTotal messages: {self.message_count}')
+
+
 class CoinbaseWallet(AuthBase):
-    def __init__(self, api_key, api_secret, api_pass):
-        self.auth_client = cbpro.AuthenticatedClient(api_key, api_secret, api_pass)
+    stablecoins = []
+
+    def __init__(self, api_key, api_secret, api_pass, api_url):
+        self.__auth_client = cbpro.AuthenticatedClient(
+            api_key, api_secret, api_pass, api_url)  # initialize client
+        self.lookback = 20
+        self.ceiling, self.floor = 30, 10
+        self.initialStopRisk = 0.98
+        self.trailingStopRisk = 0.9
         self.positions = []
-        stable_coins = []
-        
-    def __call__ (self):
-        print(json.dumps(self.auth_client.get_product_ticker("BTC-USD"), indent=2))
-        
+
+    def __call__(self):
+        print(json.dumps(self.__auth_client.get_product_ticker('BTC-USD'), indent=2))
+
     def getPositions(self):
-        accounts = self.auth_client.get_accounts()
-        
+        accounts = self.__auth_client.get_accounts()
+
         for account in accounts:
-            if float(account["balance"]) != 0:
-                self.positions.append(account)
+            self.positions.append(account)
 
         print(json.dumps(self.positions, indent=2))
-    
-    def getStableCoins(self):
-        currencies = self.auth_client.get_products()
-        
-        for currency in currencies:
-            if currency["fx_stablecoin"] and currency["quote_currency"] == "USD":
-                print(json.dumps(currency, indent=2))
+
+    def onMarketOpen(self):
+        # step 1: calculate standard deviation of data from past 30 days
+        start_date = (datetime.datetime.now() -
+                      datetime.timedelta(30)).isoformat()
+        end_date = datetime.datetime.now().isoformat()
+        historic_rates = self.__auth_client.get_product_historic_rates(
+            'BTC-USD', start=start_date, end=end_date, granularity=21600)
+
+        df = pd.DataFrame(historic_rates, columns=[
+                          'time', 'low', 'high', 'open', 'close', 'volume'])
+        std_dev = df['close'].std()
+
+    # def getHistory(self):
+    #     accounts = self.__auth_client.get_accounts()
+
+    #     for acc in accounts:
+    #         currency = acc.get('currency')
+    #         if currency == 'BTC':
+    #             acc_id = acc.get('id')
+
+    #     acc_history = self.__auth_client.get_account_history(acc_id)
+    #     for hist in acc_history:
+    #         print(json.dumps(hist, indent=2))
 
 
 def main():
     # get authentication variables
-    load_dotenv("auth_credentials.env")
-    api_key = os.getenv("API_KEY")
-    api_secret = os.getenv("API_SECRET")
-    api_pass = os.getenv("API_PASS")
-    
-    account = CoinbaseWallet(api_key, api_secret, api_pass)
-    account.getStableCoins()
-    account()
+    load_dotenv('auth_credentials.env')
+    api_key = os.getenv('API_KEY')
+    api_secret = os.getenv('API_SECRET')
+    api_pass = os.getenv('API_PASS')
+    api_url = 'https://api-public.sandbox.pro.coinbase.com'
 
-    
+    client = CoinbaseWallet(api_key, api_secret, api_pass, api_url)
+
+    client.onMarketOpen()
+
     # data = pd.DataFrame(public_client.get_products())
     # print(data.tail(6).T)
 
-    # account_holds = auth_client.get_account_holds()
-    # print(json.dumps(account_holds, indent=2))
 
-    # print(json.dumps(auth_client.get_currencies(), indent=4))
-    
 main()
-
-# 1. Loop through each of our specified currencies
-# 2. Calculate the lookback length based on the volatility(higher volatility, higher lookback length)
-# 3. The highest price in the history of the lookback is the breakout level
-# 4. If in breakout range and not in position -> buy
-# 5. else if not in breakout range -> do nothing
-# 6. For each position, set a trailing stop-loss order with a specified percentage of loss (i.e. 10 % )
-# 7. Track price of currency
-# 8. Compare current price with last stored price
-# 9. If price goes up -> hodl and update stored price
-# 10. else if price goes down, then check percentage loss
-# 11. if percentage loss >= stop-loss -> sell
-# else hodl!!
-# 12. Rinse and repeat
---
+++ test.py
@@ -0,0 +1,4 @@
+import datetime
+
+start_date = (datetime.datetime.now() - datetime.timedelta(30)).isoformat()
+print(start_date.isoformat())
